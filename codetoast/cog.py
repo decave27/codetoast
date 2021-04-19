c2e140fbf54fb5cafc8451f8143113f6a15e308f
Decave <decave27@gmail.com>
2021-04-19 21:05:34 +0900

    feat: import cogs from cogs dir

--- codetoast/cog.py
+++ codetoast/cog.py
@@ -2,12 +2,15 @@
 
 from discord.ext import commands
 
+from codetoast.cogs.main import Main
+from codetoast.cogs.filesystem import FileSystem
+
 __all__ = (
     "CodeToast",
     "CodeToast_COMMANDS",
     "setup",
 )
-CodeToast_COMMANDS = ()
+CodeToast_COMMANDS = (Main, FileSystem)
 
 
 class CodeToast(*CodeToast_COMMANDS):
